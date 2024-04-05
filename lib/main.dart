6d5e3a704cbdf9138972eb6bdb356aa0829f2873
projectStructure123 <165964814+projectStructure123@users.noreply.github.com>
2024-04-05 20:17:23 +0200

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get/get_core/src/get_main.dart';
+import 'package:untitled8/presentation/home_cat_screen/controller/home_cat_controller.dart';
+import 'package:untitled8/presentation/home_cat_screen/home_cat_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: HomeCatScreen(),
+    );
+  }
+}
+
+
+
+
